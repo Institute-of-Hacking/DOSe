6a8af74c1954c8562ae7541cdd6ad51d09c823f7
zcj <62445910+25zcj01@users.noreply.github.com>
2020-05-12 12:36:50 -0500

    Delete command-handler.bat

--- userhandlerstuff/command-handler.bat
+++
@@ -1,6 +0,0 @@
-:sakura
-start https://www.youtube.com/watch?v=jqpFjsMtCb0
-echo ^%Zhara +star Sakura. good job
-echo tell me your name on discord(noidont#5037) or on telehack. then your star will be official if you found this by looking at code and you dont tell me you did you are a dirty cheater fuck you.
-echo zcj is not allowed to get this badge due to him being a person who will see this
-exit
