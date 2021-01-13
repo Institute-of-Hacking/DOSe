76794378c00d4736ef02654e9812c0ee8d57db9f
NoIDont-Zhara <62445798+NoIDont-Zhara@users.noreply.github.com>
2021-01-13 08:25:11 -0800

    Add files via upload

--- userhandlerstuff/command-handler.bat
+++ userhandlerstuff/command-handler.bat
@@ -1,5 +1,17 @@
+@echo off
+if command == "sakura" goto sakura
+if not exist "%command%.bat" (
+    echo Command not found check spelling
+    goto eof
+) else (
+    call "%command%.bat"
+    goto eof
+)
+
+
 :sakura
 start https://www.youtube.com/watch?v=jqpFjsMtCb0
 echo ^%Zhara +star Sakura. good job
 echo zcj is not allowed to get this badge due to him being a person who will see this
+:eof
 exit
