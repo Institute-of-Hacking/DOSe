6fe504ad1d45e0b0714cc5158110aa3bf03936b6
NoIDont-Zhara <62445798+NoIDont-Zhara@users.noreply.github.com>
2021-01-26 22:32:49 -0800

    update

--- userhandlerstuff/command-handler.bat
+++
@@ -1,5 +0,0 @@
-:sakura
-start https://www.youtube.com/watch?v=jqpFjsMtCb0
-echo ^%Zhara +star Sakura. good job
-echo zcj is not allowed to get this badge due to him being a person who will see this
-exit
